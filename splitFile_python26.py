7059b34c1baebd6299a708e072c4eee7f57f5e0d
unknown <tamnguye@LFR007673.corp.capgemini.com>
2015-05-21 11:49:17 +0200

    read write csv file

---
+++ splitFile_python26.py
@@ -0,0 +1,48 @@
+'''
+/***
+Date: 20_05_2015
+Splits a CSV file into multiple pieces.
+Run on terminal: python splitFile_python26.py server_services.csv 130000
+***/
+'''
+import csv
+import sys
+import re
+import os
+
+arg_fullname = sys.argv[1]
+arg_row_limit = sys.argv[2]
+name = re.split('\.', arg_fullname)[0]
+
+def split_big_file(filename = arg_fullname, row_limit = arg_row_limit):
+	
+	outfileNumber = 1
+	outfile = None
+	
+	# open file to read
+	f = open(filename, 'rb')
+	readSource = csv.reader(f)
+	# get the header
+	header = readSource.next()
+	# separate source to 2 parts
+	for index, row in enumerate(readSource):
+		if index % int(row_limit) == 0:
+			if outfile is not None:
+				outfile.close()
+			# check if import folder is exist, if not create it
+			if not os.path.exists("import"):
+				os.makedirs("import")
+			# create sub-file name
+			outfilename = '%s-{0}.csv'.format(outfileNumber) % name
+			# open file to write
+			outfile = open(os.path.join('import',outfilename), "wb")
+			#outfile = open(outfilename, 'wb')
+			outfileNumber += 1
+			# write data
+			writer = csv.writer(outfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+			# write header for each sub-file
+			writer.writerow(header)
+		writer.writerow(row)
+
+if __name__ == "__main__":
+    split_big_file()
